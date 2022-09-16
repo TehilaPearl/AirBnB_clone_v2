5284a5ef657255b42706b11b42a44c1196e88066
Olufunke <olufunkeadejoke4@gmail.com>
2022-09-16 14:46:40 -0700

    fixed

---
+++ tests/test_models/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# File: __init__.py
+# Main Authors: Olufunke Soyemi
+# email(s): <olufunkeadejoke4@gmail.com>
+#           <>
+# Collaborators:  Yoshua Lopez - Ma Paz Quirola - Laura Socarras
+#
